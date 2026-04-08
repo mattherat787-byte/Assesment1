8702fd97c7ecf54892cf1790180163bd6cdbc289
Matthew Johnstone <mattherat787@gmail.com>
2026-04-08 14:00:53 +0200

    Add files via upload

---
+++ Assignment1_ST10511950_MatthewJohnstone.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignment1_st10511950_matthewjohnstone;
+
+/**
+ *
+ * @author matth
+ */
+public class Assignment1_ST10511950_MatthewJohnstone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
